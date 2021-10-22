7e8c5466eceb813acf471cdd6bafe381a387cd10
Rahaf Al-Jazzazi <rahafjazz@gmail.com>
2021-10-22 23:22:18 +0300

    sign up + log in auth

---
+++ src/main/java/com/sleepcoders/valhalla/security/services/UserDetailsServiceImpl.java
@@ -0,0 +1,27 @@
+package com.sleepcoders.valhalla.security.services;
+
+import com.sleepcoders.valhalla.models.User;
+import com.sleepcoders.valhalla.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User Not Found with username: " + username));
+
+        return UserDetailsImpl.build(user);
+    }
+
+}
